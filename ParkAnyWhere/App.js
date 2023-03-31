d91143b39fb81832726bf44763428234c7553421
BakedPotateo <ryanteo2991@gmail.com>
2023-04-01 00:58:24 +0800

    Fixed font error

--- ParkAnyWhere/App.js
+++ ParkAnyWhere/App.js
@@ -6,8 +6,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
-
+import * as SplashScreen from 'expo-splash-screen';
 //Screens
 import LoginScreen from './screen/LoginPage.js';
 import SignUpScreen from './screen/SignUpPage.js';
@@ -15,29 +14,54 @@ import MapScreen from './screen/MainPage.js'
 import Header from './screen/Components/Header.js';
 
 const Stack = createStackNavigator();
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = React.useState(false);
 
-  const [fontsLoaded] = Font.useFonts({
-    'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
-    'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
-  });
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+          'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
 
-  if (!fontsLoaded) {
-    return (
-      <AppLoading />
-    )
+  const onLayoutRootView = React.useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator > 
-        <Stack.Screen name="LoginPage" component={LoginScreen} options={{ headerShown: false}} />
-        <Stack.Screen name="SignUpPage" component={SignUpScreen} options={{ headerShown: false}}/>
-        
-        <Stack.Screen name="MapScreen" component={MapScreen}  options={{header: () => <Header title="ParkAnyWhere" />,}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <><View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="LoginPage" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="SignUpPage" component={SignUpScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="MapScreen" component={MapScreen} options={{ header: () => <Header title="ParkAnyWhere" />, }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View></>
   );
 }
 
