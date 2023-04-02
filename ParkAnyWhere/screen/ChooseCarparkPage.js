77e499ed68b39fe3bddcd6b57e94f9cbcf6011e6
BakedPotateo <ryanteo2991@gmail.com>
2023-04-02 19:48:26 +0800

    added WIP ChooseCarparkPage

--- ParkAnyWhere/screen/ChooseCarparkPage.js
+++ ParkAnyWhere/screen/ChooseCarparkPage.js
@@ -1,120 +1,202 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker , Callout } from 'react-native-maps';
-import * as Location from 'expo-location';
-import PriceTag from './Views/PriceTag';
-import CarParkInfo from './Views/CarParkInfo';
-const GOOGLE_MAPS_API_KEY = 'AIzaSyAk_IKcK278tmdzZEsggIpAwGkipdxiCOA';
+// import MapView, { LatLng, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+// import {
+//   StyleSheet,
+//   View,
+//   Dimensions,
+//   Text,
+//   TouchableOpacity,
+// } from "react-native";
+// import {
+//   GooglePlaceDetail,
+//   GooglePlacesAutocomplete,
+// } from "react-native-google-places-autocomplete";
+// import { GOOGLE_API_KEY } from "./environments";
+// import Constants from "expo-constants";
+// import { useRef, useState } from "react";
+// import MapViewDirections from "react-native-maps-directions";
 
-const ChooseCarparkPage = () => {
-  const [region, setRegion] = useState(null);
-  const [marker, setMarker] = useState(null);
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [carParks, setCarParks] = useState([]);
-  const markerRefs = useRef([]);
+// // https://docs.expo.dev/versions/latest/sdk/map-view/
+// // https://www.npmjs.com/package/react-native-google-places-autocomplete
+// // https://www.npmjs.com/package/react-native-maps-directions
 
-  // Get the user's current location and set it as the initial region
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+// const { width, height } = Dimensions.get("window");
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location.coords);
-      fetchNearbyCarParks(location.coords);
-    })();
-  }, []);
+// const ASPECT_RATIO = width / height;
+// const LATITUDE_DELTA = 0.02;
+// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+// const INITIAL_POSITION = {
+//   latitude: 40.76711,
+//   longitude: -73.979704,
+//   latitudeDelta: LATITUDE_DELTA,
+//   longitudeDelta: LONGITUDE_DELTA,
+// };
 
-  const fetchNearbyCarParks = async ({ latitude, longitude }) => {
-    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=parking&key=${GOOGLE_MAPS_API_KEY}`;
+// type InputAutocompleteProps = {
+//   label: string;
+//   placeholder?: string;
+//   onPlaceSelected: (details: GooglePlaceDetail | null) => void;
+// };
 
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setCarParks(data.results);
-    } catch (error) {
-      console.error('Error fetching nearby car parks:', error);
-    }
-  };
+// function InputAutocomplete({
+//   label,
+//   placeholder,
+//   onPlaceSelected,
+// }: InputAutocompleteProps) {
+//   return (
+//     <>
+//       <Text>{label}</Text>
+//       <GooglePlacesAutocomplete
+//         styles={{ textInput: styles.input }}
+//         placeholder={placeholder || ""}
+//         fetchDetails
+//         onPress={(data, details = null) => {
+//           onPlaceSelected(details);
+//         }}
+//         query={{
+//           key: GOOGLE_API_KEY,
+//           language: "pt-BR",
+//         }}
+//       />
+//     </>
+//   );
+// }
 
-  const getPriceTag = (carPark) => {
-    // Implement a function to fetch or calculate the car park price
+// export default function App() {
+//   const [origin, setOrigin] = useState<LatLng | null>();
+//   const [destination, setDestination] = useState<LatLng | null>();
+//   const [showDirections, setShowDirections] = useState(false);
+//   const [distance, setDistance] = useState(0);
+//   const [duration, setDuration] = useState(0);
+//   const mapRef = useRef<MapView>(null);
 
-    return `$${Math.floor(Math.random() * 10)}/hour`;
-  };
+//   const moveTo = async (position: LatLng) => {
+//     const camera = await mapRef.current?.getCamera();
+//     if (camera) {
+//       camera.center = position;
+//       mapRef.current?.animateCamera(camera, { duration: 1000 });
+//     }
+//   };
 
-  return (
-    <View style={styles.container}>
-      {location && (
-        <MapView
-          style={styles.map}
-          provider={PROVIDER_GOOGLE}
-          region={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          {carParks.map((carPark) => (
-            <Marker
-              key={carPark.place_id}
-              coordinate={{
-                latitude: carPark.geometry.location.lat,
-                longitude: carPark.geometry.location.lng,
-              }}
-              tracksViewChanges={false}
-            >
-              <PriceTag price={getPriceTag(carPark)} />
-              <Callout>
-                <CarParkInfo carParkInfo={carPark} />
-              </Callout>
-            </Marker>
-          ))}
-        </MapView>
-      )}
-    </View>
-  );
-};
+//   const edgePaddingValue = 70;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
+//   const edgePadding = {
+//     top: edgePaddingValue,
+//     right: edgePaddingValue,
+//     bottom: edgePaddingValue,
+//     left: edgePaddingValue,
+//   };
 
-  profilePic: {
-    left: 80,
-    width: 50,
-    height: 50,
-  },
+//   const traceRouteOnReady = (args: any) => {
+//     if (args) {
+//       // args.distance
+//       // args.duration
+//       setDistance(args.distance);
+//       setDuration(args.duration);
+//     }
+//   };
 
-  inputContainer: {
-    borderRadius: 30,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-    marginBottom: 30,
-    width: '80%',
-    height: '10%',
-    position: 'relative',
-    overflow: 'hidden',
-    elevation: 5,
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-  },
-});
+//   const traceRoute = () => {
+//     if (origin && destination) {
+//       setShowDirections(true);
+//       mapRef.current?.fitToCoordinates([origin, destination], { edgePadding });
+//     }
+//   };
 
-export default ChooseCarparkPage;
+//   const onPlaceSelected = (
+//     details: GooglePlaceDetail | null,
+//     flag: "origin" | "destination"
+//   ) => {
+//     const set = flag === "origin" ? setOrigin : setDestination;
+//     const position = {
+//       latitude: details?.geometry.location.lat || 0,
+//       longitude: details?.geometry.location.lng || 0,
+//     };
+//     set(position);
+//     moveTo(position);
+//   };
+//   return (
+//     <View style={styles.container}>
+//       <MapView
+//         ref={mapRef}
+//         style={styles.map}
+//         provider={PROVIDER_GOOGLE}
+//         initialRegion={INITIAL_POSITION}
+//       >
+//         {origin && <Marker coordinate={origin} />}
+//         {destination && <Marker coordinate={destination} />}
+//         {showDirections && origin && destination && (
+//           <MapViewDirections
+//             origin={origin}
+//             destination={destination}
+//             apikey={GOOGLE_API_KEY}
+//             strokeColor="#6644ff"
+//             strokeWidth={4}
+//             onReady={traceRouteOnReady}
+//           />
+//         )}
+//       </MapView>
+//       <View style={styles.searchContainer}>
+//         <InputAutocomplete
+//           label="Origin"
+//           onPlaceSelected={(details) => {
+//             onPlaceSelected(details, "origin");
+//           }}
+//         />
+//         <InputAutocomplete
+//           label="Destination"
+//           onPlaceSelected={(details) => {
+//             onPlaceSelected(details, "destination");
+//           }}
+//         />
+//         <TouchableOpacity style={styles.button} onPress={traceRoute}>
+//           <Text style={styles.buttonText}>Trace route</Text>
+//         </TouchableOpacity>
+//         {distance && duration ? (
+//           <View>
+//             <Text>Distance: {distance.toFixed(2)}</Text>
+//             <Text>Duration: {Math.ceil(duration)} min</Text>
+//           </View>
+//         ) : null}
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   map: {
+//     width: Dimensions.get("window").width,
+//     height: Dimensions.get("window").height,
+//   },
+//   searchContainer: {
+//     position: "absolute",
+//     width: "90%",
+//     backgroundColor: "white",
+//     shadowColor: "black",
+//     shadowOffset: { width: 2, height: 2 },
+//     shadowOpacity: 0.5,
+//     shadowRadius: 4,
+//     elevation: 4,
+//     padding: 8,
+//     borderRadius: 8,
+//     top: Constants.statusBarHeight,
+//   },
+//   input: {
+//     borderColor: "#888",
+//     borderWidth: 1,
+//   },
+//   button: {
+//     backgroundColor: "#bbb",
+//     paddingVertical: 12,
+//     marginTop: 16,
+//     borderRadius: 4,
+//   },
+//   buttonText: {
+//     textAlign: "center",
+//   },
+// });
