664e07883e5b1e95e55d1ea7d402c6f5ebd944ce
BakedPotateo <ryanteo2991@gmail.com>
2023-04-08 17:11:26 +0800

    Removed unecessary files

--- ParkAnyWhere/App.js
+++ ParkAnyWhere/App.js
@@ -1,18 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import TestPage from './screen/TestPage.js';
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+
 //Screens
 import LoginScreen from './screen/LoginPage.js';
 import SignUpScreen from './screen/SignUpPage.js';
 import MainPage from './screen/MainPage.js'
-import Header from './screen/Components/Header.js';
-import Test from './screen/test.js';
 
 //firestore
 import { auth, db , signInWithEmailAndPassword ,createUserWithEmailAndPassword ,sendEmailVerification} from './firebaseConfig';
