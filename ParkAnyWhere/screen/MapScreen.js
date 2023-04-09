88ea1de25388a56c69a9ec756a3c290b8c305db4
Bryan lee <leeweihan98@yahoo.com.sg>
2023-04-10 04:13:46 +0800

    showing parking on map from database

--- ParkAnyWhere/screen/MapScreen.js
+++ ParkAnyWhere/screen/MapScreen.js
@@ -7,6 +7,16 @@ import CarParkInfo from './Views/CarParkInfo';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 const GOOGLE_MAPS_API_KEY = 'AIzaSyAk_IKcK278tmdzZEsggIpAwGkipdxiCOA';
 
+//firebase
+import { auth, db , signInWithEmailAndPassword ,createUserWithEmailAndPassword ,sendEmailVerification} from '../firebaseConfig';
+import { collection, doc, setDoc , query, getDocs} from 'firebase/firestore';
+
+import proj4 from 'proj4';
+
+const svy21Proj = '+proj=tmerc +lat_0=1.366666666666667 +lon_0=103.8333333333333 +k=1 +x_0=28001.642 +y_0=38744.572 +ellps=WGS84 +units=m +no_defs';
+const wgs84Proj = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
+
+
 const MapScreen = () => {
   const [region, setRegion] = useState(null);
   const [marker, setMarker] = useState(null);
@@ -25,10 +35,27 @@ const MapScreen = () => {
       }
 
       let location = await Location.getCurrentPositionAsync({});
+      //testing AT NTU 1.3484010698533575, 103.68322053068263
+      location.coords.latitude = 1.3484010698533575;
+      location.coords.longitude = 103.68322053068263;
       setLocation(location.coords);
-      fetchNearbyCarParks(location.coords);
+      //fetchNearbyCarParks(location.coords);
+      fetchCarParksFromFirebase();
     })();
   }, []);
+
+  const fetchCarParksFromFirebase = async () => {
+    try {
+      const carParksCollection = collection(db, 'CarParks');
+      const carParksSnapshot = await getDocs(carParksCollection);
+      const carParksData = carParksSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      //print out the data
+      //console.log(carParksData);
+      setCarParks(carParksData);
+    } catch (error) {
+      console.error('Error fetching car parks from Firebase:', error);
+    }
+  };
 
   const fetchNearbyCarParks = async ({ latitude, longitude }) => {
     const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=parking&key=${GOOGLE_MAPS_API_KEY}`;
@@ -61,21 +88,30 @@ const MapScreen = () => {
             longitudeDelta: 0.0421,
           }}
         >
-          {carParks.map((carPark) => (
-            <Marker
-              key={carPark.place_id}
-              coordinate={{
-                latitude: carPark.geometry.location.lat,
-                longitude: carPark.geometry.location.lng,
-              }}
-              tracksViewChanges={false}
-            >
-              <PriceTag price={getPriceTag(carPark)} />
-              <Callout>
-                <CarParkInfo carParkInfo={carPark} />
-              </Callout>
-            </Marker>
-          ))}
+         {carParks.map((carPark) => {
+          // Check if the geometries array is not empty and if the coordinates property exists
+          if (carPark.geometries && carPark.geometries.length > 0 && carPark.geometries[0].hasOwnProperty("coordinates")) {
+            const coordinates = carPark.geometries[0]["coordinates"].split(',');
+            const eastings = parseFloat(coordinates[0]);
+            const northings = parseFloat(coordinates[1]);
+            const [longitude, latitude] = proj4(svy21Proj, wgs84Proj, [eastings, northings]);
+            return (
+              <Marker
+                key={carPark.id}
+                coordinate={{
+                  latitude: latitude,
+                  longitude: longitude,
+                }}
+                tracksViewChanges={true}
+              >
+                <PriceTag price={getPriceTag(carPark)} />
+                <Callout>
+                  <CarParkInfo carParkInfo={carPark} />
+                </Callout>
+              </Marker>
+            );
+          }
+        })}
         </MapView>
       )}
     </KeyboardAwareScrollView>
