1d79071b21a471b8f08e481b5c60bc7bc9e37f11
dondon1010 <113164742+dondon1010@users.noreply.github.com>
2023-04-13 17:14:49 +0800

    some debugging required still

--- ParkAnyWhere/screen/Results.js
+++ ParkAnyWhere/screen/Results.js
@@ -1,101 +1,130 @@
 // Results.js
 
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import axios from 'axios';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import * as Location from 'expo-location';
-import PriceTag from './Views/PriceTag';
 import CarParkInfo from './Views/CarParkInfo';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-const GOOGLE_API_KEY = 'AIzaSyAk_IKcK278tmdzZEsggIpAwGkipdxiCOA'; 
+// firebase
+import { auth, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification } from '../firebaseConfig';
+import { collection, doc, setDoc, query, getDocs } from 'firebase/firestore';
 
-const Results = ({ route }) => {
-  console.log("results page")
-  const { searchTerm, location } = route.params;
-  const [results, setResults] = useState([]);
+import proj4 from 'proj4';
+
+const svy21Proj = '+proj=tmerc +lat_0=1.366666666666667 +lon_0=103.8333333333333 +k=1 +x_0=28001.642 +y_0=38744.572 +ellps=WGS84 +units=m +no_defs';
+const wgs84Proj = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
+
+function FlatListScreen({ route }) {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [carParks, setCarParks] = useState([]);
 
   useEffect(() => {
-    const fetchResults = async () => {
-      try {
-        const response = await axios.get(
-          'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
-          {
-            params: {
-              key: GOOGLE_API_KEY,
-              location: `${location.latitude},${location.longitude}`,
-              radius: 1000,
-              keyword: searchTerm,
-            },
-          }
-        );
-
-        setResults(response.data.results);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
       }
-    };
 
-    if (searchTerm) {
-      fetchResults();
+      let location = await Location.getCurrentPositionAsync({});
+      //testing AT NTU 1.3484010698533575, 103.68322053068263
+      location.coords.latitude = 1.3484010698533575;
+      location.coords.longitude = 103.68322053068263;
+      setLocation(location.coords);
+      fetchCarParksFromFirebase();
+    })();
+  }, []);
+
+  const fetchCarParksFromFirebase = async () => {
+    try {
+      const carParksCollection = collection(db, 'CarParks');
+      const carParksSnapshot = await getDocs(carParksCollection);
+      const carParksData = carParksSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      const accessKey = 'f21c183d-9c02-4e50-8939-b83dad170347';
+      getCarParkAvail(accessKey, "9fV9TEJ@5cRr8x15434ZF1-30nwvS7CbMh0c015Pk5e2sc712BNfGQFtxcK39v9m72w58C39-fq7Zd165dD2+1-H7d2fuA3T7Kf4", carParksData);
+    } catch (error) {
+      console.error('Error fetching car parks from Firebase:', error);
     }
-  }, [searchTerm, location]);
-
-    const getPriceTag = (carPark) => {
-    // Implement a function to fetch or calculate the car park price
-
-    return `$${Math.floor(Math.random() * 10)}/hour`;
   };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Showing results for: {searchTerm}</Text>
+  const getCarParkAvail = async (accessKey, token, carparkData) => {
+    try {
+      const url = 'https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Availability';
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'AccessKey': accessKey,
+          'Token': token
+        }
+      });
+  
+      const data = await response.json();
+      const carParkDetails = data.Result;
+      console.log('Car park details:', carParkDetails);
+  
+      const updatedCarParksData = carparkData.map((carPark) => {
+        const foundCarPark = carParkDetails.find(cp => cp.carkparkNo === carPark.carParkNo);
+        if (foundCarPark) {
+          return {
+            ...carPark,
+            lotsAvailable: foundCarPark.lotsAvailable,
+            totalLots: foundCarPark.totalLots
+          };
+        } else {
+          return carPark;
+        }
+      });
+  
+      setCarParks(updatedCarParksData);
+    } catch (error) {
+      console.error('Error fetching car park details:', error);
+    }
+  };
+
+      const renderItem = ({ item }) => {
+        if (item.geometries && item.geometries.length > 0 && item.geometries[0].hasOwnProperty("coordinates")) {
+          const coordinates = item.geometries[0]["coordinates"].split(',');
+          const eastings = parseFloat(coordinates[0]);
+          const northings = parseFloat(coordinates[1]);
+          const [longitude, latitude] = proj4(svy21Proj, wgs84Proj, [eastings, northings]);
+
+          return (
+            <View style={styles.item}>
+              <CarParkInfo carParkInfo={item} />
+            </View>
+          );
+        }
+      };
+      
+      return (
+        <View style={styles.container}>
       <FlatList
-        data={results}
-        keyExtractor={(item) => item.place_id}
-        renderItem={({ item }) => (
-          <View style={styles.resultItem}>
-            <Text style={styles.resultName}>{item.name}</Text>
-            <Text style={styles.resultAddress}>{item.vicinity}</Text>
-            <Text style={styles.getPriceTag}>Price: ${item.price}</Text> {/* Update the property name based on the actual API response */}
-          </View>
-        )}
-      />
+        data={carParks}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+    />
     </View>
-  );
-};
+      );
+    };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-  },
-  resultItem: {
-    marginBottom: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 8,
-  },
-  resultName: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 4,
-  },
-  resultAddress: {
-    fontSize: 14,
-    marginBottom: 4,
-  },
-  resultPrice: {
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-});
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fdeeeb',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      item: {
+        backgroundColor: 'ffffff',
+        padding: 10,
+        marginVertical: 4,
+        marginHorizontal: 16,
+      },
+    });
 
-export default Results;
+    export default FlatListScreen;
 
 
 
