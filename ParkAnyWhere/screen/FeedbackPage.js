936116717ad86f6b863536420a8c3b4bde0bf546
JCBC101 <102711039+JCBC101@users.noreply.github.com>
2023-04-14 06:31:41 +0800

    FeedbackPage fully operational

--- ParkAnyWhere/screen/FeedbackPage.js
+++ ParkAnyWhere/screen/FeedbackPage.js
@@ -2,55 +2,119 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, TextInput, Pressable, ImageBackground } from "react-native";
 import StarRating from "react-native-star-rating-widget";
 import DropDownPicker from "react-native-dropdown-picker";
-import { db } from '../firebaseConfig';
 
-const FeedbackForm = ({ customerID }) => {
+import { db } from '../firebaseConfig';
+import { collection, doc , getDocs , addDoc, updateDoc , query, where} from 'firebase/firestore';
+
+const FeedbackForm = ({}) => {
   const [comment, setComment] = useState('');
   const [rating, setRating] = useState(0);
+  const [dropDownOpen, setDropDownOpen] = useState(false);
   const [carParks, setCarParks] = useState([]);
-  const [selectedCarPark, setSelectedCarPark] = useState(null);
+  const [selectedCarPark, setSelectedCarPark] = useState('');
   
+  //Hardcode CustomerID as 1
+  const customerID = 1;
+
   useEffect(() => {
     const fetchCarParks = async () => {
-      const parkingHistorySnapshot = await db.collection('ParkingHistory')
-        .where('CustomerID', '==', customerID)
-        .get();
-      const carParkIDs = parkingHistorySnapshot.docs.map(doc => doc.data().CarParkID);
+      try{
+        const parkingHistory = []; //Stores IDs of carparks in ParkingHistory under CustomerID=customerID
+        const carParksList = [];
 
-      const carParksSnapshot = await db.collection('CarParks')
-        .where('ppCode', 'in', carParkIDs)
-        .get();
-      const carParksList = carParksSnapshot.docs.map(doc => {
-        const data = doc.data();
-        return { label: data.ppName, value: data.ppCode };
-      });
+        const parkingHistoryCollection = collection(db, 'ParkingHistory');
+        const parkingHistorySnapshot = await getDocs(query(parkingHistoryCollection, where('CustomerID', '==', customerID)));
+        const parkingHistoryData = parkingHistorySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+        for (let i=0; i < parkingHistoryData.length; i++) {
+          let temp = parkingHistoryData[i].CarParkID;
+          parkingHistory.push(temp);
+          //console.log(`CarParkID: ${parkingHistory[i]}`);
+        }
+        const carParksCollection = collection(db, 'CarParks');
+        const carParksSnapshot = await getDocs(query(carParksCollection, where('ppCode', 'in', parkingHistory)));
+        const carParksData = carParksSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+        //console.log(`Length of carParksData ${carParksData.length}`)
 
-      setCarParks(carParksList);
-      setSelectedCarPark(carParksList.length > 0 ? carParksList[0].value : null);
-    };
+        for (let i=0; i < carParksData.length; i++) {
+          for(let j=0; j < parkingHistory.length; j++){
+            if(carParksData[i].ppCode == parkingHistory[j]){
+              let temp = {label:(carParksData[i].ppName), value:(parkingHistory[j])}
+              carParksList.push(temp);
+              //console.log(carParksData[i].ppCode, parkingHistory[j]);
+              //console.log(temp);
+            }
+          }
+        }
+  
+        setCarParks(carParksList);
+        setSelectedCarPark(carParksList.length > 0 ? carParksList[0].value : null);
+        
 
+      } catch(error) {
+        console.log(`Error fetching parking history for customerID: ${customerID}`);
+        
+      }
+    }
     fetchCarParks();
-  }, [customerID]);
+  }, []);
+  
+  const updateRating = async () => {
+    try{
+      const ratingCollection = collection(db, 'Rating');
+      //console.log(`Selected CarParkID: ${selectedCarPark}`);
+      const ratingSnapshot = await getDocs(query(ratingCollection, where('CarParkID', '==', selectedCarPark)));
+      const ratingData = ratingSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      
+      //Creates a new Rating document if none of selectedCarPark found
+      if(ratingData.length ==0){
+        await addDoc(ratingCollection, {
+          CarParkID: selectedCarPark,
+          AvgRating: rating,
+          ReviewCount: 1
+        });
+        console.log(`New document in Rating for CarParkID ${selectedCarPark} created!`);
+      }else{ //Else calculates new AvgRating and updates it
+        const newReviewCount = ratingData[0].ReviewCount + 1;
+        const newAvgRating = (ratingData[0].AvgRating + rating) / (newReviewCount);
+        console.log(`New average = (${ratingData[0].AvgRating} + ${rating}) / (${newReviewCount}) = ${newAvgRating}`)
 
-  const handleSubmit = (event) => {
+        await updateDoc(doc(db, 'Rating', ratingData[0].id), {
+          AvgRating: newAvgRating,
+          ReviewCount: newReviewCount
+        })
+        console.log(`AvgRating ${newAvgRating} and ReviewCount ${newReviewCount} updated successfully!`);
+      }
+
+    }catch(error){
+      console.log(`Error updating Rating for CarParkID ${selectedCarPark}`)
+    }
+  }
+  
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    
     if (rating && comment.trim() !== '' && selectedCarPark) {
-      db.collection('Feedback').add({
-        CarParkID: selectedCarPark,
-        CustomerID: customerID,
-        comment: comment,
-        rating: rating
-      })
-      .then(() => {
-        console.log('Feedback submitted successfully!');
-        setComment('');
-        setRating(0);
-      })
-      .catch((error) => {
-        console.error('Error submitting feedback: ', error);
-      });
+      try{
+        const feedbackCollection = collection(db, 'Feedback');
+        newFeedbackObject = {
+          CarParkID: selectedCarPark,
+          CustomerID: customerID,
+          comment: comment,
+          rating: rating
+        };
+        await addDoc(feedbackCollection, newFeedbackObject)
+      }catch(error){
+        console.log(`Error submitting feedback for ${selectedCarPark}`);
+      }finally{
+          console.log(`Feedback for ${selectedCarPark} submitted successfully!`);
+          setComment('');
+          setRating(0);
+          updateRating();
+      }
     }
   };
+
+
 
   return (
     <View style={styles.container}>
@@ -68,10 +132,15 @@ const FeedbackForm = ({ customerID }) => {
 
       {carParks.length > 0 ? (
         <DropDownPicker
-          items={carParks}
-          defaultValue={selectedCarPark}
-          containerStyle={{ height: 40, width: '50%' }}
           style={styles.dropDown}
+          open={dropDownOpen}
+          setOpen={setDropDownOpen}
+          value={selectedCarPark}
+          setValue={setSelectedCarPark}
+          items={carParks}
+          setItems={setCarParks}
+          containerStyle={{ height: 40, width: '50%' }}
+          
           itemStyle={{
             justifyContent: 'flex-start',
           }}
@@ -82,7 +151,11 @@ const FeedbackForm = ({ customerID }) => {
         <Text>Loading car parks...</Text>
       )}
 
-      <StarRating rating={rating} onChange={setRating} />
+      <StarRating
+      style={styles.rating} 
+      rating={rating} 
+      onChange={setRating} 
+      />
 
       <TextInput
         style={styles.reviewInput}
@@ -124,14 +197,19 @@ const styles = StyleSheet.create({
   },
 
   dropDown: {
-    width: "50%",
+    width: "100%",
     marginTop: "7%",
-    marginLeft: "25%",
-    marginBottom: "7%"
+    marginLeft: "0%",
+    marginRight: "0%",
+    marginBottom: "30%"
+  },
+
+  rating:{
+    marginTop: "35%"
   },
 
   reviewInput: {
-    height: "40%",
+    height: "30%",
     width: "50%",
     margin: "7%",
     borderWidth: 1,
